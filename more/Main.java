f276190704d0cf2eaf57997d73b8f62c5d5ca3bb
hnougba <hnougba@ulb.ac.be>
2016-12-24 00:14:55 +0100

    done

--- more/Main.java
+++ more/Main.java
@@ -11,16 +11,19 @@ public class Main {
 	}
 	public static void main(String argv[]) {
 		try {
-			if len(argv)<2:
+			int len = argv.length;
+			if (len < 2){
 				LexicalAnalyzer.main(argv);
 				String fileNameOut = nameOut(argv[0]);
-				Parser p = new Parser();
+				Parser p = new Parser(fileNameOut);
 				p.parse();
-			else if len(argv)<1:
-				System.out.println("no inputfile")
-			else:
-				System.out.println("too many arguments")
-				
+			}
+			else if (len < 1){
+				System.out.println("no inputfile");
+			}
+			else{
+				System.out.println("too many arguments");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
